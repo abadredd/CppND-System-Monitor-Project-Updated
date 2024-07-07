f0dd5072e4f065f9386ef1332d6a7afdb547a65a
abadredd <49213550+abadredd@users.noreply.github.com>
2024-07-07 16:49:28 -0400

    Update format.cpp to define ElapsedTime function
    Update format.cpp to define ElapsedTime function

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,18 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+    int hrs, mins;
+    string elapsed_time;
+
+    hrs = seconds / 3600;
+    seconds -= hrs * 3600;
+
+    mins = seconds / 60;
+    seconds -= mins * 60;
+
+    elapsed_time = std::to_string(hrs) + ":" + std::to_string(mins) + ":" + std::to_string(seconds);
+
+    return elapsed_time; 
+    
+    }
