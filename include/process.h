f72aa99b7d75b0a1ae0e56e3b6c136d80d6a2696
abadredd <49213550+abadredd@users.noreply.github.com>
2024-07-17 17:26:33 -0400

    Update process.h

--- include/process.h
+++ include/process.h
@@ -1,45 +1,57 @@
-#ifndef PROCESS_H
-#define PROCESS_H
+#ifndef SYSTEM_PARSER_H
+#define SYSTEM_PARSER_H
 
+#include <fstream>
+#include <regex>
 #include <string>
-#include <vector>
 
-using std::vector;
+namespace LinuxParser {
+// Paths
+const std::string kProcDirectory{"/proc/"};
+const std::string kCmdlineFilename{"/cmdline"};
+const std::string kCpuinfoFilename{"/cpuinfo"};
+const std::string kStatusFilename{"/status"};
+const std::string kStatFilename{"/stat"};
+const std::string kUptimeFilename{"/uptime"};
+const std::string kMeminfoFilename{"/meminfo"};
+const std::string kVersionFilename{"/version"};
+const std::string kOSPath{"/etc/os-release"};
+const std::string kPasswordPath{"/etc/passwd"};
 
-#define UTIME 0
-#define STIME 1
-#define CUTIME 2
-#define CSTIME 3
-#define STARTTIME 4
+// System
+float MemoryUtilization();
+long UpTime();
+std::vector<int> Pids();
+int TotalProcesses();
+int RunningProcesses();
+std::string OperatingSystem();
+std::string Kernel();
 
-/*
-Basic class for Process representation
-It contains relevant attributes as shown below
-*/
-class Process {
- public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
-  
-//   Process(int pid) : pid(pid) {};
-  
-  void CpuUtilization(float util){
-    cpu_usage = util;
-  } 
- 
-  Process(int& pid) : pid(pid) {};
-
-  // TODO: Declare any necessary private members
- private:
-  int pid;
-  float cpu_usage;
-  long uptime;
-  vector<int> process_util{vector<int>(10,0)};
+// CPU
+enum CPUStates {
+  kUser_ = 0,
+  kNice_,
+  kSystem_,
+  kIdle_,
+  kIOwait_,
+  kIRQ_,
+  kSoftIRQ_,
+  kSteal_,
+  kGuest_,
+  kGuestNice_
 };
+std::vector<int> CpuUtilization();
+long Jiffies();
+long ActiveJiffies();
+long ActiveJiffies(int pid);
+long IdleJiffies();
+
+// Processes
+std::string Command(int pid);
+std::string Ram(int pid);
+std::string Uid(int pid);
+std::string User(int pid);
+long int UpTime(int pid);
+};  // namespace LinuxParser
 
 #endif
