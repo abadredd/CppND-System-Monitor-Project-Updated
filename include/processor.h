e07294b4354ba5927eef133879dd3ecae49d08b6
abadredd <49213550+abadredd@users.noreply.github.com>
2024-07-16 21:22:39 -0400

    Update processor.h

--- include/processor.h
+++ include/processor.h
@@ -1,34 +1,45 @@
-#ifndef PROCESSOR_H
-#define PROCESSOR_H
+#ifndef PROCESS_H
+#define PROCESS_H
 
+#include <string>
 #include <vector>
 
 using std::vector;
 
-#define USER 0
-#define NICE 1
-#define SYSTEM 2
-#define IDLE 3
-#define IOWAIT 4
-#define IRQ 5
-#define SOFTIRQ 6
-#define STEAL 7
-#define GUEST 8
-#define GUEST_NICE 9
+#define UTIME 0
+#define STIME 1
+#define CUTIME 2
+#define CSTIME 3
+#define STARTTIME 4
 
 /*
-     user    nice   system  idle      iowait irq   softirq  steal  guest  guest_nice
-cpu  74608   2520   24433   1117073   6176   4054  0        0      0      0
-
+Basic class for Process representation
+It contains relevant attributes as shown below
 */
-class Processor {
+class Process {
  public:
-  float Utilization();  // TODO: See src/processor.cpp
+  int Pid();                               // TODO: See src/process.cpp
+  std::string User();                      // TODO: See src/process.cpp
+  std::string Command();                   // TODO: See src/process.cpp
+  float CpuUtilization();                  // TODO: See src/process.cpp
+  std::string Ram();                       // TODO: See src/process.cpp
+  long int UpTime();                       // TODO: See src/process.cpp
+  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  
+//   Process(int pid) : pid(pid) {};
+  
+  void CpuUtilization(float util){
+    cpu_usage = util;
+  } 
+ 
+  Process(int& pid) : pid(pid) {};
 
   // TODO: Declare any necessary private members
-  
  private:
-    std::vector<int> prev_cpu_util{std::vector<int>(10, 0)};
+  int pid;
+  float cpu_usage;
+  long uptime;
+  vector<int> process_util{vector<int>(10,0)};
 };
 
 #endif
